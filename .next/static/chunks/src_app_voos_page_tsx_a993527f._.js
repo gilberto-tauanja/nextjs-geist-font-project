cf4ed18d7c3bcc87a1aef354d8cddd5aded4f021
Gilberto Tauanja <129623895+gilberto-tauanja@users.noreply.github.com>
2025-05-14 06:58:35 +0200

    Add .next/static/chunks/src_app_voos_page_tsx_a993527f._.js 1747198714958

---
+++ .next/static/chunks/src_app_voos_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d2c4b801._.js",
+  "static/chunks/src_0dc1bb36._.js"
+],
+    source: "dynamic"
+});
