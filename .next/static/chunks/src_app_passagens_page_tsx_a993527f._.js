bb6d51be5d964d8dd01cea019aba57caed584453
Gilberto Tauanja <129623895+gilberto-tauanja@users.noreply.github.com>
2025-05-14 06:57:54 +0200

    Add .next/static/chunks/src_app_passagens_page_tsx_a993527f._.js 1747198673031

---
+++ .next/static/chunks/src_app_passagens_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a4c58bb3._.js",
+  "static/chunks/src_cbfed39f._.js"
+],
+    source: "dynamic"
+});
